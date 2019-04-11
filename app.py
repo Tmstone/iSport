f63d47c2aca4a3730013d21b96946c2cc3be6736
Terence Stone <terence.stone25@gmail.com>
2019-04-11 19:07:50 -0400

    added functionality to account, new & search pages

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ PW_REGEX = re.compile('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@
 #Navaigation
 nav.Bar('top', [
     nav.Item('Home', 'members'),
-    nav.Item('New', 'new_event'),
+    nav.Item('New Event', 'new_event'),
     nav.Item('Search', 'search'),
     nav.Item('Account','account'),
     nav.Item('Logout','logout')
@@ -62,35 +62,56 @@ class User(db.Model):
                 return (True, user.id)
         return (False, 'email or password incorrect')
 
+    @classmethod
+    def get_user(cls, form):
+        user = User.query.get(session['user_id'])
+        print(user)
+        return user
+###Add Option to change Password ###
+    @classmethod
+    def edit_user(cls,form):
+        user_update = User.query.get(session['user_id'])
+        user_update.first_name = form['first_name']
+        user_update.last_name = form['last_name']
+        user_update.email = form['email']
+        db.session.commit()
+        return user_update.id
+
 #### Events Table #####
 class Event(db.Model):
     __tablename__ = "events"
     id = db.Column(db.Integer, primary_key = True)
-    #add foreign_key user_id
     type = db.Column(db.String(25), nullable = False)
     location = db.Column(db.String(25), nullable = False)
     info = db.Column(db.String(125), nullable = False)
+    attendess = db.Column(db.Integer, nullable = True)
     date = db.Column(db.String(20), nullable = False)
     time = db.Column(db.String(10), nullable = False)
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
     #add relationship
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
-    user = db.relationship('User', foreign_keys=[user_id], backref="my_events", cascade="all, delete-orphan")
+    user = db.relationship('User', foreign_keys=[user_id], backref="my_events", cascade="all")
 
     @classmethod
     def add_event(cls, form):
         event = Event(
         type=form['type'],
-        location=form['info'],
+        location=form['location'],
         info=form['info'],
-        date=form['date'],
-        time=form['time']
+        attendess=form['attendees'],
+        date=form['edate'],
+        time=form['etime'],
+        user_id=session['user_id']
         )
-        db.sesson.add(event)
+        db.session.add(event)
         db.session.commit()
         return event.id
 
+    @classmethod
+    def get_event(cls):
+        events = Event.query.all()
+        return events
 
 @app.route('/')
 def index():
@@ -104,7 +125,8 @@ def new_user():
         for error in errors:
             flash(error)
         return redirect('/')
-    User.add_user(request.form)
+    user_id = User.add_user(request.form)
+    session['user_id'] = user_id
     return redirect('/dashboard')
 
 @app.route('/login', methods=['POST'])
@@ -114,32 +136,57 @@ def login():
             flash(error)
             return redirect('/')
     session['user_id'] = response
+    print(response)
     return redirect('/dashboard')
 
 @app.route('/dashboard')
 def members():
     if 'user_id' not in session:
         return redirect('/')
+    user = User.get_user(session['user_id'])
+    session['first_name'] = user.first_name
+    return render_template('dashboard.html', name = user.first_name )
 
-    return render_template('dashboard.html')
-
-#new event method & route
+###Render New Event Page###
 @app.route('/new')
 def new_event():
-    return render_template('new.html')
+    return render_template('new.html', name = session['first_name'])
+
+###Add new event###
+@app.route('/add_event', methods=['POST'])
+def add_event():
+    event = Event.add_event(request.form)
+    return redirect('/search')
 
 #search for Events
 @app.route('/search')
 def search():
-    return render_template('search.html')
+    events = Event.get_event()
+
+    return render_template('search.html',
+    name = session['first_name'],
+    fevents = events
+    )
 
 #add search query route and controller
 
-#render the account page. Add id variable
+
+#render the account page. Add id variable###
 @app.route('/user')
 def account():
-    return render_template('account.html')
+    account = User.get_user(session['user_id'])
+    return render_template('account.html',
+    name = account.first_name,
+    last= account.last_name,
+    email=account.email,
+    )
+###upadate user profile###
+@app.route('/user/update', methods=['POST'])
+def update_user():
+    profile = User.edit_user(request.form)
+    return redirect('/dashboard')
 
+####Logout####
 @app.route('/logout')
 def logout():
     session.clear()
